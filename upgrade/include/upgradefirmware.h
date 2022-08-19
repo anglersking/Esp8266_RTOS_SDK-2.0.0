2c9e332b59bda4e448060610250c29e2f7258e1b
Wanshan Pang <wanshan.pang@momenta.ai>
2022-08-19 16:56:08 +0800

    FOTA

---
+++ upgrade/include/upgradefirmware.h
@@ -0,0 +1,18 @@
+#ifndef __UPGRADEFIRMWARE_H__
+#define __UPGRADEFIRMWARE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+bool ExcuteUpgrade( char * FirmWareDownLoadPath );
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
